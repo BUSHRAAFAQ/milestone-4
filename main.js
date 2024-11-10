8bc7290bf581de20f169fd583a238a23a4f2ea93
Bushra Afaq <165610940+BUSHRS@users.noreply.github.com>
2024-11-10 18:58:43 +0500

    first commit

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
-var form = document.getElementById("resum-form");
+var form = document.getElementById("resume-form");
 var resumeDisplay = document.getElementById("resume-display");
-form.addEventListener("submit", function (evet) {
+form.addEventListener("submit", function (event) {
     event.preventDefault();
     var name = document.getElementById("name").value;
     var email = document.getElementById("email").value;
-    var call = document.getElementById("call").value;
+    var phone = document.getElementById("phone").value;
     var education = document.getElementById("education").value;
     var experience = document.getElementById("experience").value;
     var skills = document.getElementById("skills").value;
     var linkedIn = document.getElementById("linkedIn").value;
-    var resumeHTML = "\n    <h2><strong>Resume</strong></h2>\n    <h3><strong>Personal Information</strong></h3>\n    <p><strong>Name:</strong>".concat(name, "</p>\n    <p><strong>call</strong>").concat(call, "</p>\n    <p><strong>Email</strong>").concat(email, "</p>\n    <p><strong>Linkedin</strong>").concat(linkedIn, "</p>\n    <h3>Education</h3><p>").concat(education, "</p>\n    <h3>Experience</h3>").concat(experience, "</p>\n    <h3>Skills</h3>").concat(skills, "</p>\n    ");
-    if (resumeDisplayElemant) {
-        resumeDisplayElement.innerTML = resumeHTML;
+    var resumeHTML = "\n    <h2><b>Editable Resume</b></h2>\n    <h3>Personal Information</h3>\n    <p><b>Name:</b><span contenteditable=\"true\">".concat(name, "</span></p>\n     <p><b>Email:</b><span contenteditable=\"true\">").concat(email, "</span></p>\n      <p><b>Phone:</b><span contenteditable =\"true\">").concat(phone, "</span></p>\n    \n     <p>linkedIn:</b><span contentedable=\"true\"").concat(linkedIn, "</span></p>\n\n\n    <h3>Education</h3>\n    <p contentedable=\"true\">>").concat(education, "</p>\n    <h3>experience</h3>\n    <p contentedable=\"true\">").concat(experience, "</p>\n    <h3>Skills</h3>\n    <p contentedable=\"true\">").concat(skills, "</p>\n    ");
+    if (resumeDisplay) {
+        resumeDisplay.innerHTML = resumeHTML;
     }
     else {
         console.error("Resume Display element not found.");
--- main.ts
+++ main.ts
@@ -1,27 +1,33 @@
-const form=document.getElementById("resum-form")as HTMLFormElement;
+const form=document.getElementById("resume-form")as HTMLFormElement;
 const resumeDisplay=document.getElementById("resume-display")as HTMLDivElement;
 
-form.addEventListener("submit",(evet:Event) =>{
-    event?.preventDefault();
+form.addEventListener("submit",(event:Event) =>{
+    event.preventDefault();
 
     const name=(document.getElementById("name")as HTMLInputElement).value
     const email=(document.getElementById("email")as HTMLInputElement).value 
-    const call=(document.getElementById("call")as HTMLInputElement).value
+    const phone=(document.getElementById("phone")as HTMLInputElement).value
     const education=(document.getElementById("education")as HTMLInputElement).value
     const experience=(document.getElementById("experience")as HTMLInputElement).value
     const skills=(document.getElementById("skills")as HTMLInputElement).value
     const linkedIn=(document.getElementById("linkedIn")as HTMLInputElement).value
 
     const resumeHTML = `
-    <h2><strong>Resume</strong></h2>
-    <h3><strong>Personal Information</strong></h3>
-    <p><strong>Name:</strong>${name}</p>
-    <p><strong>call</strong>${call}</p>
-    <p><strong>Email</strong>${email}</p>
-    <p><strong>Linkedin</strong>${linkedIn}</p>
-    <h3>Education</h3><p>${education}</p>
-    <h3>Experience</h3>${experience}</p>
-    <h3>Skills</h3>${skills}</p>
+    <h2><b>Editable Resume</b></h2>
+    <h3>Personal Information</h3>
+    <p><b>Name:</b><span contenteditable="true">${name}</span></p>
+     <p><b>Email:</b><span contenteditable="true">${email}</span></p>
+      <p><b>Phone:</b><span contenteditable ="true">${phone}</span></p>
+    
+     <p>linkedIn:</b><span contentedable="true"${linkedIn}</span></p>
+
+
+    <h3>Education</h3>
+    <p contentedable="true">>${education}</p>
+    <h3>experience</h3>
+    <p contentedable="true">${experience}</p>
+    <h3>Skills</h3>
+    <p contentedable="true">${skills}</p>
     `;
     if(resumeDisplay){
         resumeDisplay.innerHTML = resumeHTML
